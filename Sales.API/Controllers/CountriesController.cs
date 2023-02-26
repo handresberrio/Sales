67368492c024c75238ea09bc7f36bde464b21548
afberrio <afberrio@bancolombia.com.co>
2023-02-25 19:43:27 -0500

    CRUD

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -39,16 +39,56 @@ namespace Sales.API.Controllers
         public async Task<ActionResult> PostAsyinc(Country country)
         {
             _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un país con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
         }
 
         [HttpPut]
         public async Task<ActionResult> PutAsyinc(Country country)
         {
             _context.Update(country);           // aun no se ha actualizado
-            await _context.SaveChangesAsync(); // quien hace la transaccion
-            return Ok(country);
+            try
+            {
+                await _context.SaveChangesAsync(); // quien hace la transaccion
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un país con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         [HttpDelete("{id:int}")]
